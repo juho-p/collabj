b45b1bfe7dd854f1a6cd28515c986d03d324c30d
Juho Peltonen <juhopel@gmail.com>
2015-02-21 17:06:37 +0200

    remember all drawings and display them to new clients

--- src/collabj/boards.clj
+++ src/collabj/boards.clj
@@ -3,30 +3,43 @@
 
 (def boards (ref {}))
 
+(defn new-board [client]
+  { :clients {client true} :content [] })
+
 (defn add-client-to-board
   [board-id client]
   (dosync
     (if (not (contains? @boards board-id))
-      (alter boards assoc board-id (ref {client true}))
-      (alter (@boards board-id) assoc client true))))
+      (alter boards assoc board-id (ref (new-board client)))
+      (alter (@boards board-id) assoc-in [:clients client] true))))
 
 (defn remove-client-from-board
   [board-id client]
   (dosync
-    (let [new-board (dissoc @(@boards board-id) client)]
-      (if (empty? new-board)
+    (let [new-clients (dissoc (:clients @(@boards board-id)) client)]
+      (if (empty? (:clients new-clients))
         (alter boards dissoc board-id)
-        (ref-set (@boards board-id) new-board)))))
+        (alter (@boards board-id) assoc :clients new-clients)))))
+
+(defn add-content
+  [board-id content]
+  (dosync
+    (alter (@boards board-id)
+           (fn [board]
+             (update-in board [:content] conj content)))))
 
 (defn add-client
   [req board-id]
   (with-channel req con
     (add-client-to-board board-id con)
     (println con " connected to " board-id)
+    (doseq [content (:content @(@boards board-id))]
+      (send! con content false))
     (on-receive con
                 (fn [data]
-                  (println "recv " board-id)
-                  (doseq [client @(@boards board-id)]
+                  (add-content board-id data)
+                  (println @boards)
+                  (doseq [client (:clients @(@boards board-id))]
                     (send! (key client) data false))))
     (on-close con (fn [status]
                     (remove-client-from-board board-id con)
